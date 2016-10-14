4f2dfad1b0adf2c8e076994a61a96afc149de164
duqian0228 <940856462@qq.com>
2016-10-14 15:57:17 +0800

    one

--- js/index.js
+++ js/index.js
@@ -3,45 +3,20 @@ $(function(){
     var add_item=$(".add-box .button");
     var inners=$(".inner-contents");
     var input=$("input");
-
     var todos=[];
     var com="";
     if(localStorage.table){
         todos=JSON.parse(localStorage.table);
-        $.each(todos,function(i,v){
-            creatItem(v.title);
-        });
+        creatItem();
     }else{
         localStorage.table=JSON.stringify(todos);
-    }
-    add_input.on("click",function(){
-        $(this).addClass("add-none").removeClass("add-show");
-        $(".add-list").removeClass("add-list-none").addClass("add-list-show");
-    });
-    add_item.on("click",function(){
-        add_input.removeClass("add-none").addClass("add-show");
-        $(".add-list").removeClass("add-list-show").addClass("add-list-none");
-       input[0].value="";
-    });
-    input.on("blur",function(){
-        var value=input[0].value;
-        todos.push({
-            title:value,
-            state:0,
-            isDel:0,
-            com:"complete",
-            del:"icon-guanbigongjulan"
-        });
-        localStorage.table=JSON.stringify(todos);
-        creatItem(value);
-    });
-    function creatItem(value){
-        $('<li class="inner-content item icon-font"><div class="icon-font icon-jishiben"></div><div class="title">'+value+'</div></li>').appendTo(".inner-contents")
     }
     var left=null;
     inners.on("touchstart",".inner-content",function(e){
         left=e.originalEvent.changedTouches[0].pageX;
     });
+
+    //鼠标左右移
     inners.on("touchend",".inner-content",function(e){
         var move_left=e.originalEvent.changedTouches[0].pageX;
         var x=move_left-left;
@@ -49,58 +24,77 @@ $(function(){
         // var complete=todos[index].com;
         // var del=todos[index].del;
         if(x>40 && move_left>0){
-            $(this).css({"transition":"transform .8s ease","transform":"translate3d("+x+"px,0,0)"})
-                .delay(800)
+            $(this).css({"transition":"transform .8s ease","transform":"translate3d(40px,0,0)"})
+                .delay(400)
                 .queue(function(){
-                    $(this)
+                    $(this).css({"transition":"transform .4s ease","transform":"translate3d(0,0,0)"})
+                        .find(".icons").eq(1)
                         .addClass("complete")
-                        .css({"transition":"transform .4s ease","transform":"translate3d(0,0,0)"})
                         .dequeue();
                 })
         }
         if(x<-40){
-            $(this).css({"transition":"transform .8s ease","transform":"translate3d("+x+"px,0,0)"})
-                .delay(800)
+            $(this).css({"transition":"transform .8s ease","transform":"translate3d(-40px,0,0)"})
+                .delay(400)
                 .queue(function(){
-                    $(this)
+                    $(this).css({"transition":"transform .4s ease","transform":"translate3d(0,0,0)"})
+                        .find(".icons").eq(1)
                         .removeClass("complete")
                         .addClass("icon-guanbigongjulan")
-                        .css({"transition":"transform .4s ease","transform":"translate3d(0,0,0)"})
                         .dequeue();
                 })
         }
     });
-    inners.on("touchstart",".title",function(){
-        var index=$(this).index();
+    //出现input框，失去焦点添加
+    add_input.on("click",function(){
+        $(this).addClass("add-none").removeClass("add-show");
+        $(".add-list").removeClass("add-list-none").addClass("add-list-show");
+    });
+    input.on("blur",function(){
+        var value=input[0].value;
+        if(value){
+            todos.push({
+                title:value,
+                state:0,
+                isDel:0,
+                com:"complete",
+                del:"icon-guanbigongjulan"
+            });
+            localStorage.table=JSON.stringify(todos);
+            creatItem(value);
+        }
+    });
+
+    //添加单条记录
+    add_item.on("click",function(){
+        add_input.removeClass("add-none").addClass("add-show");
+        $(".add-list").removeClass("add-list-show").addClass("add-list-none");
+       input[0].value="";
+        refresh();
+
+    });
+
+    function creatItem(){
+        $.each(todos,function(i,v){
+            $('<li class="inner-content item"><div class="icon-jishi icon-font icon-jishiben"></div><div class="title">'+v.title+'</div><div class="icons change icon-xiugai"></div> <div class="icons queding"></div></li>').appendTo(".inner-contents")
+        });
+    }
+
+    //修改记录
+    inners.on("touchstart",".change",function(){
+        var index=$(this).closest("li").index();
         add_input.addClass("add-none").removeClass("add-show");
         $(".add-list").removeClass("add-list-none").addClass("add-list-show");
-        input[0].value=$(this).text();
+        input[0].value=$(this).prev(".title").text();
         input.on("blur",function(){
             todos[index].title=input[0].value;
+            todos[index].state=1;
             localStorage.table=JSON.stringify(todos);
+
         });
     });
-    inners.on("touchstart",".inner-content",function(e){
-        left=e.originalEvent.changedTouches[0].pageX;
-    });
-    inners.on("touchstart",".inner-content",function(e){
-        left=e.originalEvent.changedTouches[0].pageX;
-    });
-    if(inners.find(".inner-content").length){
-        $('<li class="inner-content delete-inner ">删除全部</li>').appendTo(".inner-contents")
-    }
-    //删除全部
-    var delete_inner=$(".delete-inner");
-    delete_inner.on("click",function(){
-        $(".inner-content")
-            .addClass("delete")
-            .delay(800)
-            .queue(function(){
-                $(this).remove().dequeue();
-            });
-        todos=[];
-        localStorage.table=JSON.stringify(todos);
-    })
+
+    //删除单条记录
     inners.on("click",".icon-guanbigongjulan",function(){
         $(this).closest(".item").css({"transition":"transform .8s ease","transform":"translate3d(-100%,0,0)"})
             .delay(800)
@@ -110,5 +104,51 @@ $(function(){
         var index=$(this).closest(".item").index();
         todos.splice(index,1);
         localStorage.table=JSON.stringify(todos);
-    })
+        if(todos.length===0){
+            refresh();
+        }
+    });
+
+    //选中单条记录
+    inners.on("click", ".icon-jishi",function(){
+        $(this).toggleClass("icon-jishiben").toggleClass("icon-queding1");
+        $(this).closest(".item").find(".queding").toggleClass("icon-guanbigongjulan");
+    });
+
+    //全选，删除全部记录
+    $(".bianji").on("click",function(){
+        if($(".icon-jishi").length===0){
+            $(".bianji span").removeClass("quanxuan")
+                .addClass("noquan").removeClass("active");
+        }else{
+            $(this).find("span").removeClass("noquan").toggleClass("quanxuan").toggleClass("active");
+            $(".icon-jishi").toggleClass("icon-jishiben").toggleClass("icon-queding1");
+            $(".icon-jishi").closest(".item").find(".queding").toggleClass("icon-guanbigongjulan");
+            $('<li class="inner-content delete-inner ">删除全部</li>').appendTo(".inner-contents")
+            $(".delete-inner").on("click",function(){
+                $(".inner-content")
+                    .addClass("delete")
+                    .delay(400)
+                    .queue(function(){
+                        $(this).remove().dequeue();
+                        $(".bianji").find("span").removeClass("quanxuan").removeClass("active").addClass("noquan");
+                    });
+                todos=[];
+                localStorage.table=JSON.stringify(todos);
+            })
+        }
+    });
+
+    //刷新
+    function refresh(){
+        history.go(-0); //刷新
+    }
+
+    //调整全选的状态
+    if(todos.length===0){
+        $(".bianji span").removeClass("quanxuan")
+            .addClass("noquan").removeClass("active");
+    }else{
+        $(".bianji span").removeClass("noquan").addClass("quanxuan");
+    }
 });
